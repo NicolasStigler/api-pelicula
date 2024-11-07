849047a0600328942948b93dd65011663f429b3d
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-07 09:37:03 -0500

    Update CrearPelicula.py

--- CrearPelicula.py
+++ CrearPelicula.py
@@ -4,24 +4,58 @@ import os
 
 def lambda_handler(event, context):
     # Entrada (json)
-    print(event) # Log json en CloudWatch
-    tenant_id = event['body']['tenant_id']
-    pelicula_datos = event['body']['pelicula_datos']
-    nombre_tabla = os.environ["TABLE_NAME"]
-    # Proceso
-    uuidv4 = str(uuid.uuid4())
-    pelicula = {
-        'tenant_id': tenant_id,
-        'uuid': uuidv4,
-        'pelicula_datos': pelicula_datos
-    }
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(nombre_tabla)
-    response = table.put_item(Item=pelicula)
-    # Salida (json)
-    print(pelicula) # Log json en CloudWatch
-    return {
-        'statusCode': 200,
-        'pelicula': pelicula,
-        'response': response
-    }
+    try:
+        log_info = {
+            "tipo": "INFO",
+            "log_datos": {
+                "event": event
+            }
+        }
+        print(json.dumps(log_info)) # Log json en CloudWatch
+    
+        tenant_id = event['body']['tenant_id']
+        pelicula_datos = event['body']['pelicula_datos']
+        nombre_tabla = os.environ["TABLE_NAME"]
+        
+        # Proceso
+        uuidv4 = str(uuid.uuid4())
+        pelicula = {
+            'tenant_id': tenant_id,
+            'uuid': uuidv4,
+            'pelicula_datos': pelicula_datos
+        }
+
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(nombre_tabla)
+        response = table.put_item(Item=pelicula)
+        
+        log_info = {
+            "tipo": "INFO",
+            "log_datos": {
+                "mensaje": "Pelicula creada correctamente",
+                "pelicula": pelicula,
+                "response": response
+            }
+        }
+
+        # Salida (json)
+        print(json.dumps(log_info)) # Log json en CloudWatch
+        return {
+            'statusCode': 200,
+            'pelicula': pelicula,
+            'response': response
+        }
+    except Exception as e:
+        log_error = {
+            "tipo": "ERROR",
+            "log_datos": {
+                "mensaje": str(e),
+                "event": event
+            }
+        }
+        print(json.dumps(log_error)) # Log json en CloudWatch
+
+        return {
+            'statusCode': 500,
+            'mensaje_error': str(e)
+        }
