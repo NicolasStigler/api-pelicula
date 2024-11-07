012e433835caa32ddf268542273df34d5d996978
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-07 10:02:14 -0500

    Update CrearPelicula.py

--- CrearPelicula.py
+++ CrearPelicula.py
@@ -5,57 +5,47 @@ import os
 def lambda_handler(event, context):
     # Entrada (json)
     try:
-        log_info = {
-            "tipo": "INFO",
-            "log_datos": {
-                "event": event
-            }
-        }
-        print(json.dumps(log_info)) # Log json en CloudWatch
-    
         tenant_id = event['body']['tenant_id']
         pelicula_datos = event['body']['pelicula_datos']
         nombre_tabla = os.environ["TABLE_NAME"]
-        
-        # Proceso
-        uuidv4 = str(uuid.uuid4())
-        pelicula = {
-            'tenant_id': tenant_id,
-            'uuid': uuidv4,
-            'pelicula_datos': pelicula_datos
-        }
-
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table(nombre_tabla)
-        response = table.put_item(Item=pelicula)
-        
-        log_info = {
-            "tipo": "INFO",
-            "log_datos": {
-                "mensaje": "Pelicula creada correctamente",
-                "pelicula": pelicula,
-                "response": response
-            }
-        }
-
-        # Salida (json)
-        print(json.dumps(log_info)) # Log json en CloudWatch
-        return {
-            'statusCode': 200,
-            'pelicula': pelicula,
-            'response': response
-        }
-    except Exception as e:
-        log_error = {
+    except KeyError as e:
+        error = {
             "tipo": "ERROR",
-            "log_datos": {
-                "mensaje": str(e),
-                "event": event
-            }
+            "log_datos": event["body"]
         }
-        print(json.dumps(log_error)) # Log json en CloudWatch
-
+        print(error)
         return {
-            'statusCode': 500,
-            'mensaje_error': str(e)
+            'statusCode': 400,
+            'error': str(e),
+            'message': "Faltan datos"
         }
+
+    # Proceso
+    uuidv4 = str(uuid.uuid4())
+    pelicula = {
+        'tenant_id': tenant_id,
+        'uuid': uuidv4,
+        'pelicula_datos': pelicula_datos
+    }
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(nombre_tabla)
+    response = table.put_item(Item=pelicula)
+        
+    log_info = {
+        "tipo": "INFO",
+        "log_datos": {
+            "mensaje": "Pelicula creada correctamente",
+            "pelicula": pelicula,
+            "response": response
+        }
+    }
+
+    # Salida (json)
+    print(log_info) # Log json en CloudWatch
+    return {
+        'statusCode': 200,
+        'pelicula': pelicula,
+        'response': response
+    }
+    
